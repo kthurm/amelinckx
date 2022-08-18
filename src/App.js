b59540696f337fe923599d214c7e19766339a7b1
Kara Thurmond <kthurmond@Karas-Air.lan>
2022-08-17 20:43:33 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen items-center">
+      <header className="">
+        <div className="font-bold text-3xl text-purple-500">
+          Andrew Amelinckx
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
