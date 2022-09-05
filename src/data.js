0f266d5119bba57b0a2167c58abed7b286713806
Kara Thurmond <kthurmond@Karas-Air.lan>
2022-09-05 16:03:58 -0400

    Adds Nav bar

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const links = [
+  {
+    name: "Books",
+    path: "/Books",
+  },
+
+  {
+    name: "Bio",
+    path: "/Bio",
+  },
+
+  {
+    name: "Events",
+    path: "/Events",
+  },
+
+  {
+    name: "Press",
+    path: "/Press",
+  },
+];
