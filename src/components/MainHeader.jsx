908d46a110e3ec5c31bac351174dd31839c7ee2a
Kara Thurmond <kthurmond@Karas-Air.lan>
2023-02-11 17:13:27 -0500

    adjusts mobile photo size

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -7,11 +7,11 @@ const MainHeader = () => {
     <div>
       <header className="fixed w-full flex justified-between h-[450px] lg:overflow-hidden z-10">
         <div
-          className="flex w-2/3 relative bg-cover bg-black/50 bg-blend-darken bg-right-top lg:bg-left-top"
+          className="flex w-1/2 md:w-2/3 relative bg-cover bg-black/50 bg-blend-darken bg-right-top lg:bg-left-top"
           style={{ backgroundImage: `url('${Syncom}')` }}
         ></div>
         <div
-          className="relative w-1/3 bg-cover bg-no-repeat bg-left-top"
+          className="relative w-1/2 md:w-1/3 bg-cover bg-no-repeat bg-left-top"
           style={{ backgroundImage: `url('${Author}')` }}
         >
           <div className="absolute bottom-1 right-3 text-[10px] text-gray-200">
