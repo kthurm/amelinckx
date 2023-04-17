340ce8dd23eabacd6ac8e3961834471a5d3fe236
Kara Thurmond <kthurmond@Karas-Air.lan>
2023-04-17 07:31:45 -0400

    adds events

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -2,8 +2,44 @@ const Events = () => {
   return (
     <div className="container-lg flex flex-col space-y-6">
       <h1>Events</h1>
-      <h2>Satellite Boy Book Launch!</h2>
+      <h2>
+        <em>Satellite Boy</em> West Coast Book Launch
+      </h2>
       <p>
+        <a href="https://storiesla.com/events">Stories Books & Cafe</a>
+        <br />
+        7:00pm <br />
+        May 19, 2023
+      </p>
+      <p>
+        Andrew Amelinckx in conversation with author and editor{" "}
+        <a href="http://danlopezauthor.com/">Dan Lopez </a>
+      </p>
+      <p>
+        1716 W Sunset Blvd
+        <br />
+        Los Angeles, CA 90026
+        <br />
+        213-413-3733
+      </p>
+      <div className="spacer h-2"></div>
+      <hr className="text-[#ddd]" />
+      <div className="spacer h-2"></div>
+      <p className="py-3">
+        <a href="https://eepurl.com/cDQVtH">Sign up for updates</a> and we’ll
+        let you know where to catch Andrew next. We promise not to share your
+        information with anyone else.
+      </p>
+      <div className="spacer h-2"></div>
+      <hr className="text-[#ddd]" />
+      <div className="spacer h-2"></div>
+      <h1>Past Events</h1>
+      <h2>2023</h2>
+      <p>
+        <span class="font-bold">
+          <em>Satellite Boy</em> Book Launch!
+        </span>
+        <br />
         <a href="https://www.instagram.com/nycitiot/">Citiot</a>
         <br />
         404 Main St. Catskill, NY
@@ -15,18 +51,12 @@ const Events = () => {
         4pm-7pm
         <br />
         Andrew will be in conversation about Satellite Boy with filmmaker and
-        writer Niva Dorell at 5pm.
+        writer{" "}
+        <a href="https://nivadorell.substack.com/?utm_source=substack&utm_medium=web&utm_campaign=reader2&utm_source=%2Fsearch%2FNiva%2520Dorell&utm_medium=reader2">
+          Niva Dorell
+        </a>{" "}
+        at 5pm.
       </p>
-
-      <p className="py-5">
-        <a href="https://eepurl.com/cDQVtH">Sign up for updates</a> and we’ll
-        let you know where to catch Andrew next. We promise not to share your
-        information with anyone else.
-      </p>
-      <div className="spacer h-4"></div>
-      <hr className="text-[#ddd]" />
-      <div className="spacer h-4"></div>
-      <h1>Past Events</h1>
       <h2>2019</h2>
       <p>West Nyack Free Library, West Nyack, NY</p>
       <p>Claverack Free Library, Claverack, NY</p>
@@ -43,13 +73,9 @@ const Events = () => {
       <hr className="text-[#ddd]" />
       <h2>2017</h2>
       <p>Klyne Esopus Museum, Ulster Park, NY</p>
-
       <p>Jay Street Bar (Hosted by The Culture Club), Brooklyn, NY</p>
-
       <p>Barnes & Nobles, Kingston, NY</p>
-
       <p>The Golden Notebook, Woodstock, NY</p>
-
       <p>Oblong Books, Rhinebeck, NY.</p>
     </div>
   );
